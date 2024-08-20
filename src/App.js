3a77895120af839c0f697883625070d350872dc1
BlesonPhilip <blesonphilip21@gmail.com>
2024-08-20 22:37:41 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import Header from './Header';
 import { Data } from "./Data";
-
+import Card from './Card';
 function App() {
-
-
-
-
   return (
     
     <div className="App">
       <Header />
       <div style={{display:'flex',flexWrap:'wrap'}}>
       {Data.map((item)=>(
-        <>
-      
-        <img style={{objectFit:'contain'}}width="100" padding="10px" src={item.image} alt="images" />
-        <p>{item.name}</p>
+        <>    
+        {/* <img style={{objectFit:'contain',width: "100px" ,padding:"10px" ,backgroundColor:'red'}} src={item.image} alt="images" /> */}
+        {/* <p>{item.name}</p> */}
+        <Card image={item.image} 
+              offerPrice = {item.offerPrice}
+              actualPrice = {item.actualPrice}
+              rating = {item.rating}
+              name = {item.name} />
         </>
       
       ))}
-    
-              
-            
       </div>
+      
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,10 @@ import {Favorite} from '@mui/icons-material'
 import React from 'react';
 import './Card.css';
 
-const Card = ({offerPrice,image,name,rating,actualPrice}) => {
+const Card = ({image , offerPrice , actualPrice , rating , name}) => {
   return (
-    <div className='card'>
-        <div className='card__heart'>
+    <div className='card' style={{border:'1px solid black' , margin:'4px'}} >
+        <div className='card__heart' >
           <Favorite />
         </div>
         <div className='card__image'>
@@ -15,11 +15,11 @@ const Card = ({offerPrice,image,name,rating,actualPrice}) => {
 
         
         <div className='card__details'>
-          <p className="title">Adidas</p>
+          <p className="title">{name}</p>
           <p>running shoes</p>
           <span className="span1">{offerPrice}</span>
           <span className="span2">{actualPrice}</span>
-          <span className="span3">56%</span>
+          <span className="span3">{rating}%</span>
         </div>
         <div className='card__size'>
           <p>Size<span>6,7,8,9</span></p>
